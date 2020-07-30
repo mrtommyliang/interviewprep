d9318024ff17ea3455d4eddd3e2a3000fa212e94
Tommy Liang <tommyliangsays@gmail.com>
2020-07-30 14:40:13 -0400

    update

--- Linked Lists/populatingRight.js
+++ Linked Lists/populatingRight.js
@@ -33,5 +33,20 @@ The number of nodes in the given tree is less than 6000.
 */
 
 const connect = (root) => {
-  
+  if (!root) return root
+  let queue = [root]
+  let level = []
+
+  while (queue.length) {
+    const node = queue.shift()
+    node.next = queue[0] || null
+    if (node.left != null) {level.push(node.left)}
+    if (node.right != null) {level.push(node.right) }
+
+    if (queue.length === 0) {
+      queue = level
+      level = []
+    }
+  }
+  return root
 }
