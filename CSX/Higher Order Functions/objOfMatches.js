e7384f9597060779fc982e7df4eedda5ef610407
Tommy Liang <tommyliangsays@gmail.com>
2020-08-14 14:40:49 -0400

    HOF

---
+++ CSX/Higher Order Functions/objOfMatches.js
@@ -0,0 +1,32 @@
+/*
+Construct a function objOfMatches that accepts two arrays and a callback. objOfMatches will build an object and return it. To build the object, objOfMatches will test each element of the first array using the callback to see if the output matches the corresponding element (by index) of the second array. If there is a match, the element from the first array becomes a key in an object, and the element from the second array becomes the corresponding value.
+*/
+
+const objOfMatches = (arr1, arr2, callback) => {
+  let result = {}
+  let curKey, curVal
+  for (let i = 0; i < arr1.length; i++) {
+    let curKey = arr1[i]
+    let curVal = arr2[i]
+    if (callback(curKey) === curVal) {
+      result[curKey] = callback(curVal)
+    }
+  }
+  return result
+}
+
+// Uncomment these to check your work!
+const arr1 = ['hi', 'howdy', 'bye', 'later', 'hello'];
+const arr2 = ['HI', 'Howdy', 'BYE', 'later', 'HELLO'];
+function uppercaser(str) { 
+  return str.toUpperCase()
+}
+console.log(objOfMatches(arr1, arr2, uppercaser)); 
+/*
+should log: 
+{ 
+  hi: 'HI', 
+  bye: 'BYE', 
+  hello: 'HELLO' 
+}
+*/
