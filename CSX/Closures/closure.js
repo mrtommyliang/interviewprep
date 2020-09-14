1ef5143bbd3a170082417456c304d72e9f4d13f9
Tommy Liang <tommyliangsays@gmail.com>
2020-09-14 14:15:09 -0400

    updates

--- CSX/Closures/closure.js
+++ CSX/Closures/closure.js
@@ -228,12 +228,12 @@ const cycleIterator = (array) => {
   return inner
 }
 
-const threeDayWeekend = ['Fri', 'Sat', 'Sun'];
-const getDay = cycleIterator(threeDayWeekend);
-console.log(getDay()); // should log: 'Fri'
-console.log(getDay()); // should log: 'Sat'
-console.log(getDay()); // should log: 'Sun'
-console.log(getDay()); // should log: 'Fri'
+  const threeDayWeekend = ['Fri', 'Sat', 'Sun'];
+  const getDay = cycleIterator(threeDayWeekend);
+  console.log(getDay()); // should log: 'Fri'
+  console.log(getDay()); // should log: 'Sat'
+  console.log(getDay()); // should log: 'Sun'
+  console.log(getDay()); // should log: 'Fri'
 
 
 
