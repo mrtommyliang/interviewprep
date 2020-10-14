8c5e7bf270628221980fbff0a1018946b21a1781
Tommy Liang <tommyliangsays@gmail.com>
2020-10-14 18:35:18 -0400

    closures

--- CSX/Closures/closure2.js
+++ CSX/Closures/closure2.js
@@ -10,8 +10,8 @@ const createFunction = () => {
   return inner
 }
 
-const hello = createFunction();
-console.log(hello()); // => should console.log('hello');
+const hello = createFunction()
+console.log(hello()) // => should console.log('hello')
 
 /***************************************************************************************/
 
@@ -27,10 +27,10 @@ const createFunctionPrinter = (val) => {
   return inner
 }
 
-const printSample = createFunctionPrinter('sample');
-console.log(printSample()); // => should console.log('sample');
-const printHello = createFunctionPrinter('hello');
-console.log(printHello()); // => should console.log('hello');
+const printSample = createFunctionPrinter('sample')
+console.log(printSample()) // => should console.log('sample')
+const printHello = createFunctionPrinter('hello')
+console.log(printHello()) // => should console.log('hello')
 
 /***************************************************************************************/
 
@@ -41,16 +41,16 @@ Uncomment those lines of code. Try to deduce the output before executing. Now we
 */
 
 const outer = () => {
-  let counter = 0; // this variable is outside incrementCounter's scope
+  let counter = 0 // this variable is outside incrementCounter's scope
   const incrementCounter = () => {
-    counter++;
+    counter++
     return ['counter', counter]
   }
-  return incrementCounter;
+  return incrementCounter
 }
 
-const willCounter = outer();
-const jasCounter = outer();
+const willCounter = outer()
+const jasCounter = outer()
 
 console.log(willCounter())
 console.log(willCounter())
@@ -65,18 +65,18 @@ const addByX = (input) => {
   return inner
 }
 
-const addByTwo = addByX(2);
-console.log(addByTwo(1));
-console.log(addByTwo(2));
-console.log(addByTwo(3));
+const addByTwo = addByX(2)
+console.log(addByTwo(1))
+console.log(addByTwo(2))
+console.log(addByTwo(3))
 
-const addByThree = addByX(3);
-console.log(addByThree(1));
-console.log(addByThree(2));
+const addByThree = addByX(3)
+console.log(addByThree(1))
+console.log(addByThree(2))
 
-const addByFour = addByX(4);
-console.log(addByFour(4));
-console.log(addByFour(5));
+const addByFour = addByX(4)
+console.log(addByFour(4))
+console.log(addByFour(5))
 
 /***************************************************************************************/
 
@@ -99,9 +99,9 @@ const once = (cb) => {
 }
 
 const onceFunc = once(addByTwo)
-console.log(onceFunc(4));
-console.log(onceFunc(10));
-console.log(onceFunc(9001));
+console.log(onceFunc(4))
+console.log(onceFunc(10))
+console.log(onceFunc(9001))
 
 /***************************************************************************************/
 
@@ -121,11 +121,11 @@ const after = (count, cb) => {
   return inner
 }
 
-const called = function () { console.log('hello') };
-const afterCalled = after(3, called);
-console.log(afterCalled());
-console.log(afterCalled());
-console.log(afterCalled());
+const called = function () { console.log('hello') }
+const afterCalled = after(3, called)
+console.log(afterCalled())
+console.log(afterCalled())
+console.log(afterCalled())
 
 /***************************************************************************************/
 
@@ -137,8 +137,8 @@ Write a function rollCall that accepts an array of names and returns a function.
 const rollCall = (array) => {
   let counter = 0
   const inner = () => {
-    if (counter < array.length) console.log(array[counter++]);
-    if (counter === array.length) console.log("Everyone accounted for");
+    if (counter < array.length) console.log(array[counter++])
+    if (counter === array.length) console.log("Everyone accounted for")
   }
   return inner
 }
@@ -189,11 +189,11 @@ const saveOutput = (cb, pw) => {
   return inner
 }
 
-const multiplyBy2 = function (num) { return num * 2; };
-const multBy2AndLog = saveOutput(multiplyBy2, 'boo');
-console.log(multBy2AndLog(2)); // => should log 4
-console.log(multBy2AndLog(9)); // => should log 18
-console.log(multBy2AndLog('boo')); // => should log { 2: 4, 9: 18 }
+const multiplyBy2 = function (num) { return num * 2 }
+const multBy2AndLog = saveOutput(multiplyBy2, 'boo')
+console.log(multBy2AndLog(2)) // => should log 4
+console.log(multBy2AndLog(9)) // => should log 18
+console.log(multBy2AndLog('boo')) // => should log { 2: 4, 9: 18 }
 
 /***************************************************************************************/
 
@@ -212,12 +212,12 @@ const cycleIterator = (array) => {
 }
 
 
-const threeDayWeekend = ['Fri', 'Sat', 'Sun'];
-const getDay = cycleIterator(threeDayWeekend);
-console.log(getDay()); // => should log 'Fri'
-console.log(getDay()); // => should log 'Sat'
-console.log(getDay()); // => should log 'Sun'
-console.log(getDay()); // => should log 'Fri'
+const threeDayWeekend = ['Fri', 'Sat', 'Sun']
+const getDay = cycleIterator(threeDayWeekend)
+console.log(getDay()) // => should log 'Fri'
+console.log(getDay()) // => should log 'Sat'
+console.log(getDay()) // => should log 'Sun'
+console.log(getDay()) // => should log 'Fri'
 
 /***************************************************************************************/
 
@@ -233,9 +233,9 @@ const defineFirstArg = (cb, ...val) => {
   return inner
 }
 
-const subtract = function (big, small) { return big - small; };
-const subFrom20 = defineFirstArg(subtract, 20);
-console.log(subFrom20(5)); // => should log 15
+const subtract = function (big, small) { return big - small }
+const subFrom20 = defineFirstArg(subtract, 20)
+console.log(subFrom20(5)) // => should log 15
 
 /***************************************************************************************/
 
@@ -257,9 +257,9 @@ const createSecretHolder = (secret) => {
 }
 
 obj = createSecretHolder(5)
-console.log(obj.getSecret());
-console.log(obj.setSecret(2));
-console.log(obj.getSecret(2));
+console.log(obj.getSecret())
+console.log(obj.setSecret(2))
+console.log(obj.getSecret(2))
 
 /***************************************************************************************/
 
@@ -277,12 +277,12 @@ const callTimes = () => {
   return inner
 }
 
-let myNewFunc1 = callTimes();
-let myNewFunc2 = callTimes();
-console.log(myNewFunc1());
-console.log(myNewFunc1());
-console.log(myNewFunc2());
-console.log(myNewFunc2());
+let myNewFunc1 = callTimes()
+let myNewFunc2 = callTimes()
+console.log(myNewFunc1())
+console.log(myNewFunc1())
+console.log(myNewFunc2())
+console.log(myNewFunc2())
 
 /***************************************************************************************/
 
@@ -303,12 +303,12 @@ const russianRoulette = (num) => {
 }
 
 
-const play = russianRoulette(3);
-console.log(play()); // => should log 'click'
-console.log(play()); // => should log 'click'
-console.log(play()); // => should log 'bang'
-console.log(play()); // => should log 'reload to play again'
-console.log(play()); // => should log 'reload to play again'
+const play = russianRoulette(3)
+console.log(play()) // => should log 'click'
+console.log(play()) // => should log 'click'
+console.log(play()) // => should log 'bang'
+console.log(play()) // => should log 'reload to play again'
+console.log(play()) // => should log 'reload to play again'
 
 /***************************************************************************************/
 
@@ -350,13 +350,13 @@ const average = () => {
   return inner
 }
 
-const avgSoFar = average();
-console.log(avgSoFar());    // => should log 0
-console.log(avgSoFar(4));   // => should log 4
-console.log(avgSoFar(8));   // => should log 6
-console.log(avgSoFar());    // => should log 6
-console.log(avgSoFar(12));  // => should log 8
-console.log(avgSoFar());    // => should log 8
+const avgSoFar = average()
+console.log(avgSoFar())    // => should log 0
+console.log(avgSoFar(4))   // => should log 4
+console.log(avgSoFar(8))   // => should log 6
+console.log(avgSoFar())    // => should log 6
+console.log(avgSoFar(12))  // => should log 8
+console.log(avgSoFar())    // => should log 8
 
 /***************************************************************************************/
 
@@ -371,19 +371,19 @@ The returned function should return true if the first elements (of each sub-arra
 
 */
 
-const capLastTestCases = [];
-capLastTestCases.push(['hello', 'hellO']);
-capLastTestCases.push(['goodbye', 'goodbyE']);
-capLastTestCases.push(['howdy', 'howdY']);
-const capLastAttempt1 = (str) => str.toUpperCase();
-const capLastAttempt2 = (str) => str.slice(0, -1) + str.slice(-1).toUpperCase();
+const capLastTestCases = []
+capLastTestCases.push(['hello', 'hellO'])
+capLastTestCases.push(['goodbye', 'goodbyE'])
+capLastTestCases.push(['howdy', 'howdY'])
+const capLastAttempt1 = (str) => str.toUpperCase()
+const capLastAttempt2 = (str) => str.slice(0, -1) + str.slice(-1).toUpperCase()
 
 /***************** APPROACH 1 ******************/
 
 const makeFuncTester = (array) => {
   const inner = (cb) => {
     let result = []
-    for (let i = 0; i < array.length; i++) {
+    for (let i = 0 i < array.length i++) {
       result.push(cb(array[i][0]) === array[i][1])
     }
     return !(result.includes(false))
@@ -400,13 +400,13 @@ const makeFuncTester2 = (array) => {
   return inner
 }
 
-const shouldCapitalizeLast = makeFuncTester(capLastTestCases);
-const shouldCapitalizeLast2 = makeFuncTester2(capLastTestCases);
-console.log(shouldCapitalizeLast(capLastAttempt1)); // => should log false
-console.log(shouldCapitalizeLast(capLastAttempt2)); // => should log true
+const shouldCapitalizeLast = makeFuncTester(capLastTestCases)
+const shouldCapitalizeLast2 = makeFuncTester2(capLastTestCases)
+console.log(shouldCapitalizeLast(capLastAttempt1)) // => should log false
+console.log(shouldCapitalizeLast(capLastAttempt2)) // => should log true
 
-console.log(shouldCapitalizeLast2(capLastAttempt1)); // => should log false
-console.log(shouldCapitalizeLast2(capLastAttempt2)); // => should log true
+console.log(shouldCapitalizeLast2(capLastAttempt1)) // => should log false
+console.log(shouldCapitalizeLast2(capLastAttempt2)) // => should log true
 
 /***************************************************************************************/
 
@@ -435,23 +435,108 @@ const makeHistory = (limit) => {
   let memory = []
   const inner = (input) => {
     if (input !== "undo") {
-      if (memory.length > limit) memory.shift();
-      memory.push(input);
-      return input + " done";
+      if (memory.length > limit) memory.shift()
+      memory.push(input)
+      return input + " done"
     } else {
-      if (memory.length === 0) return "nothing to do";
-      let remove = memory.pop();
-      return remove + " undone";
+      if (memory.length === 0) return "nothing to do"
+      let remove = memory.pop()
+      return remove + " undone"
     }
   }
   return inner
 }
-const myActions = makeHistory(2);
-console.log(myActions('jump')); // => should log 'jump done'
-console.log(myActions('undo')); // => should log 'jump undone'
-console.log(myActions('walk')); // => should log 'walk done'
-console.log(myActions('code')); // => should log 'code done'
-console.log(myActions('pose')); // => should log 'pose done'
-console.log(myActions('undo')); // => should log 'pose undone'
-console.log(myActions('undo')); // => should log 'code undone'
-console.log(myActions('undo')); // => should log 'nothing to undo'
+const myActions = makeHistory(2)
+console.log(myActions('jump')) // => should log 'jump done'
+console.log(myActions('undo')) // => should log 'jump undone'
+console.log(myActions('walk')) // => should log 'walk done'
+console.log(myActions('code')) // => should log 'code done'
+console.log(myActions('pose')) // => should log 'pose done'
+console.log(myActions('undo')) // => should log 'pose undone'
+console.log(myActions('undo')) // => should log 'code undone'
+console.log(myActions('undo')) // => should log 'nothing to undo'
+
+/***************************************************************************************/
+
+/*
+Challenge 19
+Inspect the commented out test cases carefully if you need help to understand these instructions.
+
+Create a function blackjack that accepts an array (which will contain numbers ranging from 1 through 11), and returns a DEALER function. The DEALER function will take two arguments (both numbers), and then return yet ANOTHER function, which we will call the PLAYER function.
+On the FIRST invocation of the PLAYER function, it will return the sum of the two numbers passed into the DEALER function.
+
+On the SECOND invocation of the PLAYER function, it will return either:
+
+the first number in the array that was passed into blackjack PLUS the sum of the two numbers passed in as arguments into the DEALER function, IF that sum is 21 or below, OR
+the string 'bust' if that sum is over 21.
+
+If it is 'bust', then every invocation of the PLAYER function AFTER THAT will return the string 'you are done!' (but unlike 'bust', the 'you are done!' output will NOT use a number in the array). If it is NOT 'bust', then the next invocation of the PLAYER function will return either:
+
+the most recent sum plus the next number in the array (a new sum) if that new sum is 21 or less, OR
+the string 'bust' if the new sum is over 21.
+
+And again, if it is 'bust', then every subsequent invocation of the PLAYER function will return the string 'you are done!'. Otherwise, it can continue on to give the next sum with the next number in the array, and so forth.
+You may assume that the given array is long enough to give a 'bust' before running out of numbers.
+
+BONUS: Implement blackjack so the DEALER function can return more PLAYER functions that will each continue to take the next number in the array after the previous PLAYER function left off. You will just need to make sure the array has enough numbers for all the PLAYER functions.
+*/
+
+/*
+  https://dev.to/internettradie/mastering-hard-parts-of-javascript-closure-iv-2i4k
+
+  The most important concept here is that we have two closures here, one inside the other. The outer function can be thought of as the deck of cards, the function inside that can be thought of as the dealer, and the one inside that can be thought of as the players. Thinking logically about blackjack, a dealer can deal many players, and a single deck of cards can be used in many dealings. Thinking like this should clarify where each variable that acts as memory should reside.
+
+  Implementing the bonus part just required realising that we needed two different counters, one for the dealer and one for the players, and then to modify the logic very slightly to count correctly.
+*/
+
+const blackjack = (array) => {
+  let dealerCount = 0
+  const dealer = (a, b) => {
+    let playerCount = 0
+    let total = a + b
+    const player = () => {
+      if (total === "bust") return "you are done!"
+      dealerCount++
+      playerCount++
+      if (playerCount === 1) return total
+      total += array[dealerCount - 2]
+      if (total > 21) {
+        total = "bust"
+        dealerCount--
+      }
+      return total
+    }
+    return player
+  }
+  return dealer
+}
+
+// /*** DEALER ***/
+const deal = blackjack([2, 6, 1, 7, 11, 4, 6, 3, 9, 8, 9, 3, 10, 4, 5, 3, 7, 4, 9, 6, 10, 11])
+
+// /*** PLAYER 1 ***/
+const i_like_to_live_dangerously = deal(4, 5)
+console.log(i_like_to_live_dangerously()) // => should log 9
+console.log(i_like_to_live_dangerously()) // => should log 11
+console.log(i_like_to_live_dangerously()) // => should log 17
+console.log(i_like_to_live_dangerously()) // => should log 18
+console.log(i_like_to_live_dangerously()) // => should log 'bust'
+console.log(i_like_to_live_dangerously()) // => should log 'you are done!'
+console.log(i_like_to_live_dangerously()) // => should log 'you are done!'
+
+// /*** PLAYER 2 ***/
+const i_TOO_like_to_live_dangerously = deal(2, 2)
+console.log(i_TOO_like_to_live_dangerously()) // => should log 4
+console.log(i_TOO_like_to_live_dangerously()) // => should log 15
+console.log(i_TOO_like_to_live_dangerously()) // => should log 19
+console.log(i_TOO_like_to_live_dangerously()) // => should log 'bust'
+console.log(i_TOO_like_to_live_dangerously()) // => should log 'you are done!
+console.log(i_TOO_like_to_live_dangerously()) // => should log 'you are done!
+
+// /*** PLAYER 3 ***/
+const i_ALSO_like_to_live_dangerously = deal(3, 7)
+console.log(i_ALSO_like_to_live_dangerously()) // => should log 10
+console.log(i_ALSO_like_to_live_dangerously()) // => should log 13
+console.log(i_ALSO_like_to_live_dangerously()) // => should log 'bust'
+console.log(i_ALSO_like_to_live_dangerously()) // => should log 'you are done!
+console.log(i_ALSO_like_to_live_dangerously()) // => should log 'you are done!
